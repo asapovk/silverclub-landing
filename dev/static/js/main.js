cbec9a75919bd7fefe81bcfc8f16a39f25364b4f
asapovk <asapovk@gmail.com>
2017-07-13 01:17:28 +0300

    alpha

--- dev/static/js/main.js
+++ dev/static/js/main.js
@@ -1,2 +1,10 @@
 ;
 // Начинать писать отсюда!!!!
+
+$(document).ready(function(){
+  var menu = $('.header__menu');
+  $('.header__burger').on('click', function(){
+    $(this).toggleClass('active');
+    menu.toggleClass('show');
+  })
+});
